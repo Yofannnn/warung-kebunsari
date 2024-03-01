939a647a85f7bf74ec5fc9d10c1485f725114232
Yofannnn <yofanapril17@gmail.com>
2024-03-01 13:10:23 +0700

    resize img

--- assets/script/about.js
+++ assets/script/about.js
@@ -4,6 +4,7 @@ const imgScroll = document.querySelector('.imgScroll .bg');
 window.addEventListener('scroll' , function() {
     let scroll = window.scrollY;
     header.firstElementChild.style.transform = `scale(${1 + (scroll*.0004)})`;
+    header.lastElementChild.firstElementChild.style.transform = `translateY(${0 + (scroll*.2)}px)`;
     if(window.innerWidth < 575.98){
         imgScroll.style.transform = `translateY(${(this.innerHeight - scroll*.23)}px)`
     } else imgScroll.style.transform = `translateY(${(this.innerHeight - scroll*.255)}px)`;
